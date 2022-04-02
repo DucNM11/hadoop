47bfe48284fa2507ddea78b41d361af92aa745b6
Minh Duc Nguyen <mduc458@gmail.com>
2022-04-02 21:24:15 +0100

    Update doc and plotting code

--- a_part.py
+++ a_part.py
@@ -11,7 +11,8 @@ from mrjob.job import MRJob
 from datetime import datetime as dt
 
 
-class lab3(MRJob):
+class part_a(MRJob):
+
     def mapper(self, _, line):
 
         fields = line.split(",")
@@ -20,29 +21,30 @@ class lab3(MRJob):
             if len(fields) == 7:
                 timestamp = dt.fromtimestamp(int(fields[6]))
                 mth_yr = f"{timestamp.month}/{timestamp.year}"
-                yield(mth_yr, (int(fields[3]), 1))
+                yield (mth_yr, (int(fields[3]), 1))
         except:
             pass
 
     def combiner(self, key, values):
+
         count = 0
         total = 0
+
         for val in values:
             total += val[0]
             count += val[1]
-        yield(key, (total, count))
+        yield (key, (total, count))
 
     def reducer(self, key, values):
+
         count = 0
         total = 0
+
         for val in values:
             total += val[0]
             count += val[1]
-        yield(key, (total, count))
+        yield (key, (total, count))
 
-# this class will define two additional methods: the mapper method goes here
-# and the reducer method goes after this line
-# this part of the python script tells to actually run the defined MapReduce job. Note that lab3 is the name of the class
 
 if __name__ == '__main__':
-    lab3.run()
+    part_a.run()
---
+++ plot_a_part.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# Import neccessary packages
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import data into pandas object
+df = pd.read_csv('out_2.txt', sep=",", names=['date', 'value', 'tnx'])
+
+# Process df data types
+df['value'] = df['value'].astype('float')
+df['date'] = df['date'].astype('datetime64')
+df = df.set_index('date')
+df = df.sort_index()
+
+# Define title and x/y labels for the first chart for number of transactions per month
+plt.title('Number of transactions per month\n', fontsize=20, fontweight='bold')
+plt.xlabel('Year-Month', fontsize=16)
+plt.ylabel('Number of transactions', fontsize=16)
+
+# Plot bar chart and show
+plt.bar(df.index, df.tnx, width=10)
+plt.show()
+
+# Define title and x/y labels for the second chart for average value of transactions per month
+plt.title('Average value of transactions per month\n',
+          fontsize=20,
+          fontweight='bold')
+plt.xlabel('Year-Month', fontsize=16)
+plt.ylabel('Average value of transactions', fontsize=16)
+
+# Plot bar chart and show
+plt.bar(df.index, df.value / df.tnx, width=10)
+plt.show()
